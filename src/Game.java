1de73c0210086ac5cb6aef82cff5cfedba3edcec
Nikolay Boshnakov <n_boshnakov@abv.bg>
2019-10-08 20:41:24 +0300

    Game is playable. To be polished

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Game {
@@ -15,6 +18,7 @@ public class Game {
             this.boardState[i] = new int[3];
         }
         this.turns = 0;
+        this.winner = null;
     }
 
     public Player getPlayer1() {
@@ -35,6 +39,82 @@ public class Game {
 
     public int getTurns() {
         return turns;
+    }
+    public void play()throws IOException {
+        while(!isFull() && winner == null){
+            int playersTurn = turns % 2;
+            Player player;
+            if(playersTurn == 0){
+                player = player1;
+            }
+            else{
+                player = player2;
+            }
+            System.out.println(player + "'s turn: ");
+            drawBoard();
+            selectCell(playersTurn);
+            checkForWinner(player);
+            turns++;
+        }
+    }
+
+
+    private boolean isFull() {
+        for (int[] array : boardState) {
+            if (Arrays.stream(array).anyMatch(e -> e == 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private void checkForWinner(Player player) {
+        for(int num = 0; num < 3;num++) {
+            if (boardState[num][0] != 0){
+                if(boardState[num][0] == boardState[num][1] && boardState[num][1] == boardState[num][2]) {
+                    printWinner(player);
+                }
+            }
+            if (boardState[0][num] != 0){
+                if(boardState[0][num] == boardState[1][num] && boardState[1][num] == boardState[2][num]){
+                    printWinner(player);
+                }
+            }
+            if(boardState[1][1] != 0){
+                if (boardState[0][0] == boardState[1][1] && boardState[1][1] == boardState[2][2]){
+                    printWinner(player);
+                }
+                if (boardState[0][2] != 0 && boardState[0][2] == boardState[1][1] && boardState[1][1] == boardState[2][0]){
+                    printWinner(player);
+                }
+            }
+        }
+    }
+
+    private void printWinner(Player player) {
+        winner = player;
+        drawBoard();
+        System.out.println("Winner is " + player);
+    }
+
+    private void selectCell(int playersTurn) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int row, col;
+        do {
+            System.out.println("Please choose an empty place on which to place your symbol: ");
+           do {
+               System.out.print("Row(1, 2 or 3): ");
+               String text = reader.readLine();
+               row = Integer.parseInt(text) - 1;
+           } while(row > 2 || row < 0);
+           do{
+               System.out.print("Col(1, 2 or 3): ");
+               String text1 = reader.readLine();
+               col = Integer.parseInt(text1) - 1;
+           } while(col > 2 || col < 0);
+        } while(boardState[row][col] != 0);
+        boardState[row][col] = playersTurn + 1;
     }
 
     public void drawBoard(){
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Game game1 = new Game("Nick", "Miki");
-        game1.drawBoard();
+        game1.play();
     }
 }
